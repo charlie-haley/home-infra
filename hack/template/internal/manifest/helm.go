7ae751fee2cb1dc3f20ff8384126030019a72fea
Charlie Haley <charlie.haley@hotmail.com>
2024-08-12 21:17:34 +0100

    add support for direct git chart

--- hack/template/internal/manifest/helm.go
+++ hack/template/internal/manifest/helm.go
@@ -1,6 +1,8 @@
 package manifest
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta2"
@@ -10,6 +12,15 @@ import (
 )
 
 func (m *Manifest) ProcessHelm(app string, namespace string, appDir string) error {
+	sourceKind := sourcev1.HelmRepositoryKind
+	repoURL := m.Helm.Repo
+
+	// Check if the repo URL is prefixed with git://
+	if strings.HasPrefix(repoURL, "git://") {
+		sourceKind = sourcev1.GitRepositoryKind
+		repoURL = "https://" + strings.TrimPrefix(repoURL, "git://")
+	}
+
 	// Create HelmRelease
 	hr := &helmv2.HelmRelease{
 		ObjectMeta: metav1.ObjectMeta{
@@ -25,7 +36,7 @@ func (m *Manifest) ProcessHelm(app string, namespace string, appDir string) erro
 					Chart:   m.Helm.Chart,
 					Version: m.Helm.Version,
 					SourceRef: helmv2.CrossNamespaceObjectReference{
-						Kind: sourcev1.HelmRepositoryKind,
+						Kind: sourceKind,
 						Name: app,
 					},
 				},
@@ -44,26 +55,57 @@ func (m *Manifest) ProcessHelm(app string, namespace string, appDir string) erro
 	}
 	m.Kustomize = append(m.Kustomize, "helm-release.yaml")
 
-	// Create HelmRepository
-	repo := &sourcev1.HelmRepository{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      app,
-			Namespace: namespace,
-		},
-		Spec: sourcev1.HelmRepositorySpec{
-			Interval: metav1.Duration{
-				Duration: 15 * time.Minute,
+	if sourceKind == sourcev1.HelmRepositoryKind {
+		repo := &sourcev1.HelmRepository{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      app,
+				Namespace: namespace,
 			},
-			URL: m.Helm.Repo,
-		},
-	}
-	repo.SetGroupVersionKind(schema.GroupVersionKind{Group: "source.toolkit.fluxcd.io", Version: "v1beta2", Kind: "HelmRepository"})
+			Spec: sourcev1.HelmRepositorySpec{
+				Interval: metav1.Duration{
+					Duration: 15 * time.Minute,
+				},
+				URL: repoURL,
+			},
+		}
+		repo.SetGroupVersionKind(schema.GroupVersionKind{Group: "source.toolkit.fluxcd.io", Version: "v1beta2", Kind: "HelmRepository"})
 
-	err = createFile(repo, "/helm-repository.yaml", appDir)
-	if err != nil {
-		return err
-	}
-	m.Kustomize = append(m.Kustomize, "helm-repository.yaml")
+		err = createFile(repo, "/helm-repository.yaml", appDir)
+		if err != nil {
+			return err
+		}
+		m.Kustomize = append(m.Kustomize, "helm-repository.yaml")
 
-	return nil
+		return nil
+	}
+
+	if sourceKind == sourcev1.GitRepositoryKind {
+		gitRepo := &sourcev1.GitRepository{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      app,
+				Namespace: namespace,
+			},
+			Spec: sourcev1.GitRepositorySpec{
+				Interval: metav1.Duration{
+					Duration: 15 * time.Minute,
+				},
+				URL: repoURL,
+				Reference: &sourcev1.GitRepositoryRef{
+					Branch: "main", // Default to 'main' branch
+				},
+			},
+		}
+		gitRepo.SetGroupVersionKind(schema.GroupVersionKind{Group: "source.toolkit.fluxcd.io", Version: "v1beta2", Kind: "GitRepository"})
+
+		err = createFile(gitRepo, "/git-repository.yaml", appDir)
+		if err != nil {
+			return err
+		}
+		m.Kustomize = append(m.Kustomize, "git-repository.yaml")
+
+		return nil
+	}
+
+	return fmt.Errorf("unsupported source kind: %s", sourceKind)
+
 }
